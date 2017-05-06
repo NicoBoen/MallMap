1eebc869ae59ffbe3ee48f735997f9cd8bc6da01
iosdev <iosdev@labb611-09.local>
2017-05-06 12:59:16 +0700

    Adding MapKit

--- MallMap/TableViewController.swift
+++ MallMap/TableViewController.swift
@@ -10,6 +10,7 @@ import UIKit
 
 var mall = ["Mall Taman Anggrek", "Mall Puri Indah", "Mall SMS"]
 var mallDesc = ["Mall Taman Anggrek is located in West Jakarta", "Mall Puri Indah is located in West Jakarta", "Mall SMS is located in Tangerang"]
+var mallCoords = [(-6.178685,106.791852), (-6.184723, 106.753095), (-6.241013, 106.628237)]
 var myIndex = 0
 
 
@@ -36,7 +37,15 @@ class TableViewController: UITableViewController {
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         myIndex = indexPath.row
         performSegue(withIdentifier: "segue", sender: self)
+
     }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        var test = mallCoords[myIndex]
+        let pass = segue.destination as! ViewController
+        pass.coord = test
+    }
+   
     
    
 }
--- MallMap/ViewController.swift
+++ MallMap/ViewController.swift
@@ -7,31 +7,52 @@
 //
 
 import UIKit
+import MapKit
 
 class ViewController: UIViewController {
+    var coord = (0.0,0.0)
+    
 
     
     @IBOutlet weak var titleLabel: UILabel!
     @IBOutlet weak var myImage: UIImageView!
     @IBOutlet weak var descLabel: UILabel!
+    
     @IBAction func backButton(_ sender: Any) {
         self.dismiss(animated: true, completion: nil)
     }
+    @IBOutlet weak var mapHaha: MKMapView!
+        
+    
 
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        
+        let annotation = MKPointAnnotation()
+        
         
         let fileName = mall[myIndex] + ".jpeg"
         print(fileName)
         
+        let span:MKCoordinateSpan = MKCoordinateSpanMake(0.005, 0.005)
+        let location: CLLocationCoordinate2D = CLLocationCoordinate2DMake(coord.0, coord.1)
+        let region:MKCoordinateRegion = MKCoordinateRegionMake(location, span)
+
+        mapHaha.setRegion(region, animated: true)
         
         titleLabel.text = mall[myIndex]
         descLabel.text = mallDesc[myIndex]
         myImage.image = UIImage(named: fileName)
+        //let coord = mallCoords[myIndex]
+        
+        
+        
+
+        
+        
+        
         
         
     }
